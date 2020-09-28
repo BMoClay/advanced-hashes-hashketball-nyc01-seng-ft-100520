dc9f04176f6406f5afb4dbc39725d4b2b39b2bc4
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-28 04:28:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,7 +199,7 @@ def big_shoe_rebounds
   game_hash.each do |key, value|
     value[:players].each do |inner_value|
       binding.pry
-      if inner_value[:shoe] < big_shoes
+      if inner_value[:shoe] > big_shoes
        inner_value[:shoe] = big_shoes
       end
       binding.pry
