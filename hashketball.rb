8662b4df083dc7d66082b73d6d420f22ed957104
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-28 04:19:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,8 +188,7 @@ def player_stats(player)
    game_hash.each do |key, value|
     value[:players].each do |inner_value|
       if inner_value[:player_name] == player
-        binding.pry
-        #return inner_value[:shoe]
+            stats  << inner_value
       end
     end
     stats
